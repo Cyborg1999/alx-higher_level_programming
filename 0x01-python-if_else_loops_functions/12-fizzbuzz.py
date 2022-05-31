94de871c07a4dabc5e4211c90411178d1e63559b
Cyborg1999 <andrewk.tonui@gmail.com>
2022-05-31 19:33:29 +0300

    buzz cut

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for number in range(1, 100):
         if number % 3 == 0 and number % 5 == 0:
-            print(f"{number:d} Fizz Buzz")
+            print(f"{number:d}. Fizz Buzz")
         elif number % 3 == 0:
-            print(f"{number:d} Fizz")
+            print(f"{number:d}. Fizz")
         elif number % 5 == 0:
-            print(f"{number:d} Buzz")
+            print(f"{number:d}. Buzz")
         else:
-            print(f"{number:d}")
+            print(f"{number:d}.")
