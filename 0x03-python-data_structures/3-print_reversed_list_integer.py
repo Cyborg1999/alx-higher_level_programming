f2807ea4bb66dae9ae9e65c3480169bcf23082b2
Cyborg1999 <andrewk.tonui@gmail.com>
2022-06-05 23:10:34 +0300

    files and readme

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from click import pass_context
+
+
+def print_reversed_list_integer(my_list=[]):
+
+    if not my_list:
+        pass
+    else:
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print("{my_list:d}".format(my_list[i]))
