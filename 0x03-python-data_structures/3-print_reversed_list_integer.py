bfafc510d9a55b236c369133741d4cc458b3ad05
Cyborg1999 <andrewk.tonui@gmail.com>
2022-06-06 23:05:49 +0300

    reversed digits

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-from click import pass_context
-
-
 def print_reversed_list_integer(my_list=[]):
 
-    if not my_list:
-        pass
-    else:
-        my_list.reverse()
-        for i in range(len(my_list)):
-            print("{:d}".format(my_list[i]))
+    if my_list:
+        for i in reversed(my_list):
+            print("{:d}".format(i))
