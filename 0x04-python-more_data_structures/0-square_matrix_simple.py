00e16ca0e0d62337c58ad1cfc96c717f6357f0db
Cyborg1999 <andrewk.tonui@gmail.com>
2022-06-08 02:54:42 -0700

    style correction

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def square_matrix_simple(matix = []):
+def square_matrix_simple(matix=[]):
     new_matrix = matix.copy()
 
     for i in range(len(matix)):
         new_matrix[i] = list(map(lambda x: x**2, matix[i]))
-    
+
     return (new_matrix)
