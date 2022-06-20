31c30485b26b471db8bb9abc602f7a446db28f3e
Cyborg1999 <andrewk.tonui@gmail.com>
2022-06-20 09:05:40 -0700

    list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-from tkinter import Y
-
-
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
         try:
-            result = my_list_1[i] /my_list_2[i]
+            result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
             result = 0
@@ -18,5 +15,5 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         finally:
             new_list.append(result)
-        
+
     return (new_list)
