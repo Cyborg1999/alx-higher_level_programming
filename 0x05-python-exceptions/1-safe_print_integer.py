cf28e53fb47493da180e788aab807b5735c4b6ba
Cyborg1999 <andrewk.tonui@gmail.com>
2022-06-20 17:57:24 +0300

    safe print integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import re
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except:
+        return (False)
+    else:
+        return (True)
