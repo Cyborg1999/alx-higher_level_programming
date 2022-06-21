0065f9d09146ed0f9c84f02493840e1c0ab3f183
Cyborg1999 <andrewk.tonui@gmail.com>
2022-06-21 12:26:24 +0300

    [Task]: Find Area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Square:
+    """A Class that defines a square by its size
+    """
+    def __init__(self, size=0):
+        """ Method to initialise the square object
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Method to calculate the area of a square
+            returns the value area
+        """
+        return (self.__size ** 2)
