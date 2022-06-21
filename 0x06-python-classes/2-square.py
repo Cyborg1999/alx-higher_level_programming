4837f6c210e04811235a972671e1ed69bc5977ac
Cyborg1999 <andrewk.tonui@gmail.com>
2022-06-21 23:01:19 +0300

    [Fix]: retry

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-""" A Class that defines a square by its size """
+"""Define a class Square."""
 
 
 class Square:
-    """ A Class that defines a square by its size
-    """
+    """Represent a square."""
+
     def __init__(self, size=0):
-        """Method to initialize the square object
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an Integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be  >= 0")
-        else:
-            self.__size  = size
+            raise ValueError("size must be >= 0")
+        self.__size = size
