638c790e37df03869d809dfa5abc1110f5e4dab4
Cyborg1999 <andrewk.tonui@gmail.com>
2022-06-28 23:02:48 +0300

    Magic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -3,10 +3,10 @@
 
 
 def magic_string():
-    """ Magic Strings 
+    """ Magic Strings
     Returns:
-        print 
+        print
     """
-    
+
     magic_string.n = getattr(magic_string, 'n', 0) + 1
     return ("BestSchool," * (magic_string.n - 1) + "BestSchool")
