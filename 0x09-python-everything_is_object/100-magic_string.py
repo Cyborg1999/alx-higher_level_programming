7e3db243f164a406ca2cb560e2dd41a357955164
Cyborg1999 <andrewk.tonui@gmail.com>
2022-06-28 23:06:58 +0300

    Fix magic file

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-""" Magic Class """
-
-
 def magic_string():
-    """ Magic Strings
-    Returns:
-        print
-    """
-
     magic_string.n = getattr(magic_string, 'n', 0) + 1
-    return ("BestSchool," * (magic_string.n - 1) + "BestSchool")
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
